a2cb027b4d894e0b6ff04cad8f089908a0e9a790
Matt <odonnell004@gmail.com>
2020-11-13 01:19:49 -0500

    show assertions on each instance

--- tests/test_main.py
+++ tests/test_main.py
@@ -19,5 +19,12 @@ class InstanceMockTest(unittest.TestCase):
         main.Thing().baz()
         self.assertEqual(len(instance_mock.instances), 2)
 
+    @mock.patch("instance.Instance", new_callable=tests.helpers.instance_mock.create_mock)
+    def test_instance_mock_assert_not_called(self, instance_mock):
+        main.Thing().baz()
+        instance_mock.instances[0].bar.assert_not_called()
+
+        instance_mock.last_instance().bar.assert_called()
+        instance_mock.last_instance().zed.assert_called()
 
 
